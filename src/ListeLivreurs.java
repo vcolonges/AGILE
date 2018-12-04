35a8c06da1ee92538f5a160d7554084e4bdbb10c
Emilie Borghino <emilie.borghino@insa-lyon.fr>
2018-12-04 11:40:59 +0100

    ajout classe liste livreurs

---
+++ src/ListeLivreurs.java
@@ -0,0 +1,24 @@
+import modele.Livreur;
+
+import java.awt.*;
+
+public class ListeLivreurs {
+
+    public static final Livreur[] livreurs = {
+            new Livreur("Robert",new Color(230,25,75)),
+            new Livreur("Jeanne",new Color(60,180,75)),
+            new Livreur("Alfred",new Color(0,130,200)),
+            new Livreur("Antoine",new Color(255,225,25)),
+            new Livreur("Richard",new Color(70,240,240)),
+            new Livreur("Monique",new Color(240,50,230)),
+            new Livreur("Charles",new Color(128,0,0)),
+            new Livreur("Juliette",new Color(128,128,0)),
+            new Livreur("Manon",new Color(0,128,0)),
+            new Livreur("Camille",new Color(145,30,180)),
+            new Livreur("Mathieu",new Color(0,128,128)),
+            new Livreur("Emma",new Color(0,0,128)),
+            new Livreur("Julien",new Color(0,204,102)),
+            new Livreur("Matheo",new Color(245,130,48)),
+            new Livreur("Eloïse",new Color(210,245,60))
+    };
+}
