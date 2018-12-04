b9131b19792051bb6b2e2cb9f43b405db6fa1526
Emilie Borghino <emilie.borghino@insa-lyon.fr>
2018-12-04 10:03:38 +0100

    Ajout infos livraisons dans la popup, Heures à définir.

--- src/controleur/etat/Etat.java
+++ src/controleur/etat/Etat.java
@@ -5,15 +5,52 @@ import modele.Noeud;
 import modele.Plan;
 import vue.PopUpMenu;
 
+import javax.swing.*;
+import java.awt.*;
 
-public abstract class Etat {
+
+public class Etat {
     protected String label;
     protected Controler  controler;
 
     Etat(Controler c){this.controler = c;}
-    public abstract PopUpMenu getPopUpMenu(Plan plan, Noeud n);
+    public PopUpMenu getPopUpMenu(Plan plan, Noeud n){
+        PopUpMenu popUpMenu = new PopUpMenu();
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel label;
+        if(!plan.getLivraisons().containsKey(n.getId()))
+        {
+            label = new JLabel("Adresse : " + n.getLatitude() + " " + n.getLongitude(),SwingConstants.LEFT);
+        }
+        else
+        {
+            label = new JLabel("Adresse de livraison : " + n.getLatitude() + " " + n.getLongitude(),SwingConstants.LEFT);
+        }
+        panel.add(label);
+        popUpMenu.add(panel);
+        return popUpMenu;
+    }
 
     public String getLabel() {
         return label;
+    }
+
+    public PopUpMenu ajoutInfosLivraisonsToPopUpMenu(PopUpMenu popUpMenu){
+        JPanel panelArrivee = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel heureArrivee = new JLabel("Heure d'arrivee sur place : ");
+        panelArrivee.add(heureArrivee);
+
+        JPanel panelDuree = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel heureDuree = new JLabel("Duree sur place : ");
+        panelDuree.add(heureDuree);
+
+        JPanel panelDepart = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel heureDepart = new JLabel("Heure de depart : ");
+        panelDepart.add(heureDepart);
+
+        popUpMenu.add(panelArrivee);
+        popUpMenu.add(panelDuree);
+        popUpMenu.add(panelDepart);
+        return popUpMenu;
     }
 }
